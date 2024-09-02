2dd568a2ffb119f56ae25d0eb1ece4ac402ed825
Brian Bosh <bbosh@alternateinterior.com>
2024-09-02 14:45:13 -0600

    extract actions out of popup component

---
+++ src/actions.ts
@@ -0,0 +1,85 @@
+import { getBookmarks, getTabs } from "./chrome-apis";
+import { CmdShiftPAction } from "./command-shift-p-action";
+
+export async function getActions({
+  closePopup
+}: {
+  closePopup: () => void;
+}): Promise<CmdShiftPAction[]> {
+  const tabs = await getTabs();
+  const bookmarks = await getBookmarks();
+
+  const newActions: CmdShiftPAction[] = [];
+
+  tabs.filter(tab => !!tab.id).forEach(tab => {
+    const title = `Switch to tab: ${tab.title}`;
+    newActions.push({
+      title,
+      description: title,
+      type: "tab",
+      action: function () {
+        chrome.tabs.update(tab.id!, { active: true });
+        chrome.windows.update(tab.windowId!, { focused: true });
+        closePopup();
+      }
+    });
+  });
+  bookmarks.forEach(bookmark => {
+    const title = `Open bookmark: ${bookmark.title}`;
+    newActions.push({
+      type: 'bookmark',
+      title,
+      description: title,
+      action: function () {
+        chrome.tabs.create({ url: bookmark.url });
+        closePopup();
+      }
+    });
+  });
+
+  // Add action for opening a new tab
+  newActions.push({
+    type: 'system',
+    title: 'Open new tab',
+    description: "Creates a new tab to the configured homepage",
+    action: function () {
+      chrome.tabs.create({});
+      closePopup();
+    }
+  });
+
+  // Add action for closing the current tab
+  newActions.push({
+    type: 'system',
+    title: 'Close current tab',
+    description: "Closes the current tab",
+    action: function () {
+      chrome.tabs.query({ active: true, currentWindow: true }, function ([tab]) {
+        if (tab.id)
+          chrome.tabs.remove(tab.id);
+        closePopup();
+      });
+    }
+  });
+
+  newActions.push({
+    type: 'system',
+    title: 'Close palette',
+    description: "Close the palette without doing anything",
+    action: function () {
+      window.close();
+    }
+  });
+
+  newActions.push({
+    type: 'system',
+    title: 'Open browser shortcut keys',
+    description: "The browser defaults to control+P being print. Open the browser's shortcuts page to change this",
+    action: function () {
+      chrome.tabs.create({ url: 'chrome://extensions/shortcuts' });
+      closePopup();
+    }
+  })
+
+  return newActions;
+}
--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -3,6 +3,7 @@ import { CmdShiftPAction } from '../command-shift-p-action';
 import { getBookmarks, getTabs } from '../chrome-apis';
 import { Command } from "cmdk";
 import { GoBookmark, GoTools, GoProjectSymlink  } from "react-icons/go";
+import { getActions } from '../actions';
 
 const Popup: React.FC = () => {
   const [actions, setActions] = useState<CmdShiftPAction[]>([]);
@@ -11,87 +12,10 @@ const Popup: React.FC = () => {
   const closePopup = useCallback(() => window.close(), []);
 
   useEffect(() => {
-    const immediate = async () => {
-      const tabs = await getTabs();
-      const bookmarks = await getBookmarks();
-
-      const newActions: CmdShiftPAction[] = [];
-
-      tabs.filter(tab => !!tab.id).forEach(tab => {
-        const title = `Switch to tab: ${tab.title}`;
-        newActions.push({
-          title,
-          description: title,
-          type: "tab",
-          action: function () {
-            chrome.tabs.update(tab.id!, { active: true });
-            chrome.windows.update(tab.windowId!, { focused: true });
-            closePopup();
-          }
-        });
-      });
-      bookmarks.forEach(bookmark => {
-        const title = `Open bookmark: ${bookmark.title}`;
-        newActions.push({
-          type: 'bookmark',
-          title,
-          description: title,
-          action: function () {
-            chrome.tabs.create({ url: bookmark.url });
-            closePopup();
-          }
-        });
-      });
-
-      // Add action for opening a new tab
-      actions.push({
-        type: 'system',
-        title: 'Open new tab',
-        description: "Creates a new tab to the configured homepage",
-        action: function () {
-          chrome.tabs.create({});
-          closePopup();
-        }
-      });
-
-      // Add action for closing the current tab
-      newActions.push({
-        type: 'system',
-        title: 'Close current tab',
-        description: "Closes the current tab",
-        action: function () {
-          chrome.tabs.query({ active: true, currentWindow: true }, function ([tab]) {
-            if (tab.id)
-              chrome.tabs.remove(tab.id);
-            closePopup();
-          });
-        }
-      });
-
-      newActions.push({
-        type: 'system',
-        title: 'Close palette',
-        description: "Close the palette without doing anything",
-        action: function () {
-          window.close();
-        }
-      });
-
-      newActions.push({
-        type: 'system',
-        title: 'Open browser shortcut keys',
-        description: "The browser defaults to control+P being print. Open the browser's shortcuts page to change this",
-        action: function () {
-          chrome.tabs.create({ url: 'chrome://extensions/shortcuts' });
-          closePopup();
-        }
-      })
-
-
-      setActions(newActions);
+    (async () => {
+      setActions(await getActions({ closePopup }));
       searchBox.current?.focus();
-    }
-    immediate();
+    }).call(null);
   }, []);
 
   return (
