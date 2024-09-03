36387a01ad23c44a962b74fd7d2e19c83513f47a
Brian Bosh <bbosh@alternateinterior.com>
2024-09-02 18:43:17 -0600

    cache open tabs so the popup can render faster

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,22 @@
+import { CmdShiftPAction } from "./types/command-shift-p-action";
+
 // listens for the keyboard shortcut for and opens the popup
 chrome.commands.onCommand.addListener((command) => {
   if (command === "trigger-tab-list") {
     chrome.action.openPopup();
+  }
+});
+
+
+const actions: CmdShiftPAction[] = [];
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  if (message.type === "getActions") {
+    sendResponse(actions);
+  }
+});
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  if (message.type === "setActions") {
+    actions.splice(0, actions.length, ...message.actions);
   }
 });
