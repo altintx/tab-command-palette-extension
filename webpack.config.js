e85ee73ef61505fc13985ae475cfc6810bef325b
Brian Bosh <bbosh@alternateinterior.com>
2024-09-07 19:37:24 -0600

    remove old deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   plugins: [
-    new MiniCssExtractPlugin(),
     new CopyWebpackPlugin({
       patterns: [
         { from: 'src/assets/*.png', to: 'assets' } // Adjust the paths as necessary
@@ -26,14 +24,10 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-     },
+      }
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js', '.css', '.tsx'],
+    extensions: ['.ts', '.js', '.tsx'],
   },
 };
